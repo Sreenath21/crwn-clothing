f106f84867ff8822b1b8087c081104ad1314953d
Sreenath Paramasivam <psreenath0201@gmail.com>
2022-08-18 22:18:56 +0530

    updated shop routes and its nested routes

--- src/components/directory-item/DirectoryItem.jsx
+++ src/components/directory-item/DirectoryItem.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-import "./CategoryItem.scss";
+import "./DirectoryItem.scss";
 
 const CategoryItem = ({ category }) => {
   const { title, imageUrl } = category;
   return (
-    <div className="category-container">
+    <div className="directory-item-container">
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
       />
-      <div className="category-body-container">
+      <div className="body">
         <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
